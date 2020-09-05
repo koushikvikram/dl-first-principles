75745a3ef0184511d07f9bff5b646257fce05683
koushik-ai-distsys <gridfinsdeploy@gmail.com>
2020-09-05 07:11:42 +0530

    added more derivative plots

--- foundations/derivatives.py
+++ foundations/derivatives.py
@@ -1,6 +1,6 @@
 from typing import Callable
 from numpy import ndarray
-from functions import square
+from functions import add_10, square, cube, leaky_relu, sigmoid
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,15 +15,49 @@ def deriv(func: Callable[[ndarray], ndarray],
     return (func(input_+delta) - func(input_-delta)) / (2*delta)
 
 
-# x: ndarray = np.arange(-10, 11)
-# y = square(x)
-# y_hat = deriv(square, x)
-
 x: ndarray = np.arange(-10, 11)
+
+
 y = add_10(x)
 y_hat = deriv(add_10, x)
+plt.plot(x, y, label="add_10")
+plt.plot(x, y_hat, label="deriv of add_10")
+plt.title("add_10 vs add_10'")
+plt.legend()
+plt.show()
 
 
-plt.plot(x, y)
-plt.plot(x, y_hat)
+y = square(x)
+y_hat = deriv(square, x)
+plt.plot(x, y, label="square")
+plt.plot(x, y_hat, label="deriv of square")
+plt.title("square vs square'")
+plt.legend()
+plt.show()
+
+
+y = cube(x)
+y_hat = deriv(cube, x)
+plt.plot(x, y, label="cube")
+plt.plot(x, y_hat, label="deriv of cube")
+plt.title("cube vs cube'")
+plt.legend()
+plt.show()
+
+
+y = leaky_relu(x)
+y_hat = deriv(leaky_relu, x)
+plt.plot(x, y, label="leaky_relu")
+plt.plot(x, y_hat, label="deriv of leaky_relu")
+plt.title("leaky_relu vs leaky_relu'")
+plt.legend()
+plt.show()
+
+
+y = sigmoid(x)
+y_hat = deriv(sigmoid, x)
+plt.plot(x, y, label="sigmoid")
+plt.plot(x, y_hat, label="deriv of sigmoid")
+plt.title("sigmoid vs sigmoid'")
+plt.legend()
 plt.show()
